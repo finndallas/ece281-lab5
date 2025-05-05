7e351393b0a1d4be064d0fd060e4bc5ed65952a6
finndallas <fmd.dallas@gmail.com>
2025-05-05 14:04:13 -0600

    first try

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -17,31 +17,46 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
+ 
+ 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+ 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
-
+ 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
+ 
 entity ALU is
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
            i_B : in STD_LOGIC_VECTOR (7 downto 0);
-           i_op : in STD_LOGIC_VECTOR (2 downto 0);
+           i_op : in STD_LOGIC_VECTOR (2 downto 0); --ALU control
            o_result : out STD_LOGIC_VECTOR (7 downto 0);
            o_flags : out STD_LOGIC_VECTOR (3 downto 0));
 end ALU;
-
+ 
 architecture Behavioral of ALU is
-
+ 
+    signal sum, or_op, and_op, b_new, result : std_logic_vector(7 downto 0);
 begin
+ 
+    or_op <= i_A or i_B; --result of or
+    and_op <= i_A and i_B; --result of and
+    b_new <= (not i_B) when (i_op(0) = '1') else --flip i_B and get the sum if need to subtract
+             i_B;
+    result <= sum when ((i_op = "000") or (i_op = "001")) else --mux to select the result
+              and_op when (i_op = "010") else
+              or_op when (i_op = "011") else
+              sum;
+    o_result <= result;
+    --NZCV
+    o_flags(3) <= result(7); --N flag
+    o_flags(2) <= not (result(0) or result(1) or result(2) or result(3) or result(4) or result(5) or result(6) or result(7)); --Z flag
+    o_flags(0) <= (not i_op(1)) and (sum(7) xor i_A(7)) and (not (i_op(0) xor i_A(7) xor i_B(7))); --V flag
 
-
+ 
 end Behavioral;
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -45,11 +45,124 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
+	-- singal
+    signal w_clk : std_logic;
+    signal w_clk_TDM : std_logic;
+    signal w_clk_reset : std_logic;
+    signal w_elevator_reset  : std_logic;
+    signal w_1stelevator : std_logic_vector (3 downto 0);
+    signal w_2ndelevator : std_logic_vector (3 downto 0);
+    signal w_data : std_logic_vector (3 downto 0);
+    
+    --components
+ component sevenseg_decoder is
+        port (
+            i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
+        );
+    end component sevenseg_decoder;
+    
+    component elevator_controller_fsm is
+		Port (
+            i_clk        : in  STD_LOGIC;
+            i_reset      : in  STD_LOGIC;
+            is_stopped   : in  STD_LOGIC;
+            go_up_down   : in  STD_LOGIC;
+            o_floor : out STD_LOGIC_VECTOR (3 downto 0)		   
+		 );
+	end component elevator_controller_fsm;
+	
+	component TDM4 is
+		generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+        Port ( i_clk		: in  STD_LOGIC;
+           i_reset		: in  STD_LOGIC; -- asynchronous
+           i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		   i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		   i_D1 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		   i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		   o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+		   o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
+	   );
+    end component TDM4;
+     
+	component clock_divider is
+        generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
+                                                   -- Effectively, you divide the clk double this 
+                                                   -- number (e.g., k_DIV := 2 --> clock divider of 4)
+        port ( 	i_clk    : in std_logic;
+                i_reset  : in std_logic;		   -- asynchronous
+                o_clk    : out std_logic		   -- divided (slow) clock
+        );
+    end component clock_divider;
+	
 
   
 begin
 	-- PORT MAPS ----------------------------------------
+--generic map
 
+	   clock_divider_TDM_inst : clock_divider
+	   -- its 60Hz*4 = 240 then convert to k_div, its 4 bc 4 displaus
+	   	generic map (k_div => 208333)
+
+	     port map(
+	     --in
+	     i_clk => clk,
+	     --out
+	     i_reset => w_clk_reset,
+	     o_clk => w_clk_TDM
+	     );
+	
+	   clock_divider_inst : clock_divider
+	   generic map (k_div => 25000000) --ranbdolph told me this speed
+	     port map(
+	     --in
+	     i_clk => clk,
+	     --out
+	     i_reset => w_clk_reset,
+	     o_clk => w_clk
+	     );    
+	
+       elevator_controller_inst :  elevator_controller_fsm
+          port map(
+          --in
+            i_clk => w_clk,
+            i_reset => w_elevator_reset,
+            is_stopped => sw(0),
+            go_up_down => sw(1),
+            --out
+            o_floor => w_1stelevator
+            );
+            
+		 elevator_controller_2_inst :  elevator_controller_fsm
+          port map(
+          --in
+            i_clk => w_clk,
+            i_reset => w_elevator_reset,
+            is_stopped => sw(14),
+            go_up_down => sw(15),
+            --out
+            o_floor => w_2ndelevator
+            
+            );
+            
+		sevenseg_decoder_inst : sevenseg_decoder
+		  port map(
+		  i_Hex => w_data,
+		  o_seg_n => seg
+		  );
+		  
+		  TDM4_inst : TDM4
+        Port map ( 
+            i_clk => w_clk_TDM,
+            i_reset => btnU,
+            i_D3 => "1111",
+            i_D2 => w_2ndelevator,
+            i_D1 => "1111",
+            i_D0 => w_1stelevator,
+            o_data => w_data,
+            o_sel => an
+	   );
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
